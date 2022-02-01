030e8e57d61d764560c7d8241c51e870217185a0
fcampoa <felixcampoa@gmail.com>
2022-01-31 23:07:09 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = "Copia de Libro1.xlsx";
+                object oMissing = System.Reflection.Missing.Value;
+                Excel.Application excel = new Excel.Application();
+                Excel.Workbook wb = excel.Workbooks.Open(path, oMissing, false);
+                Excel.Worksheet worksheet = wb.Sheets["hoja1"];
+                var value = ((Excel.Range)worksheet.Cells[1, "A"]).Value; ;
+                Console.WriteLine(value);
+                //excel.Run("Division");
+                //excel.Calculate();
+                wb.Save();
+                wb.Close();
+                excel.Quit();
+                // excel.Visible = true;
+            }catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
